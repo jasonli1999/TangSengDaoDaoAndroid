a8a87806d46422ec85b2d918e429cac6c5693df7
DESKTOP-C5TOPPU\jason <jasonlee520888@gmail.com>
2024-05-26 16:30:51 +0800

    11111

---
+++ app/src/main/java/com/xinbida/tsdd/demo/push/InviteDataModel.java
@@ -0,0 +1,19 @@
+package com.xinbida.tsdd.demo.push;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class InviteDataModel implements Serializable {
+
+    @SerializedName("inviteCode")
+    private String inviteCode;
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+}
